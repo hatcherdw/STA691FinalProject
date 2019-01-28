d38f6ea8748f95cafe4adf0403c36e6b088a0f92
hatcherdw <31666998+hatcherdw@users.noreply.github.com>
2019-01-28 16:02:09 -0500

    Added pre-2015 data

--- statsRaw.R
+++ statsRaw.R
@@ -3,11 +3,23 @@ library(reshape2)
 
 data <- tribble(
   ~Year,~Certified,~"Certified-Expired",~Withdrawn,~Denied,
-  2015,41223,37715,4362,5999,
-  2016,58750,57183,4650,5560,
-  2017,42080,45529,3581,6413,
-  2018,72187,37363,3971,6255 
+    2008,21092,28113,2063,10729, 
+    2009,16041,13461,3389,5356,
+    2010,32657,37580,2736,8439,
+    2011,18714,41149,2960,10384,
+    2012,2,54579,3265,8642,
+    2013,13742,21461,3075,5874,
+    2014,35615,27018,4016,4349,
+    
+    2015,41223,37715,4362,5999,
+    2016,58750,57183,4650,5560,
+    2017,42080,45529,3581,6413,
+    2018,72187,37363,3971,6255 
 )
+
+data2 <- data %$%
+    mutate(.,Total=Certified+`Certified-Expired`+Withdrawn+Denied) %>%
+    mutate(.,Determined=Certified+`Certified-Expired`+Denied)
 
 data.m <- melt(data,id.vars="Year")
 
@@ -22,3 +34,4 @@ ggplot(data.m, aes(x = Year, y = value,fill=variable)) +
   #theme( axis.line = element_line(colour = "black",size = 1, linetype = "solid"))+
   theme(text=element_text(size=16))+
   scale_y_continuous(breaks=seq(0,1,by=0.5),labels=c("0%","50%","100%"))
+
--- wrangle.R
+++ wrangle.R
@@ -2,57 +2,77 @@ library(plyr)
 library(tidyverse)
 library(readxl)
 library(magrittr)
+library(stringr)
 
 rm(list=ls())
 
-#Read metadata file
-meta <- read_xlsx("PermDisclosureData/meta.xlsx",col_names=c("Names","Type"),
-    col_types="text")
+pre2010DataFiles <- dir("PermDisclosureData",pattern="[2][0][0][8-9].xlsx",
+    full.names=TRUE)
+pre2015DataFiles <- append(pre2010DataFiles,dir("PermDisclosureData",
+    pattern="[2][0][1][0-4].xlsx",full.names=TRUE))
 
-#Get file names
-dataFiles <- dir("PermDisclosureData",pattern="\\d{4}",full.names=TRUE)
+pre2015Missing = c("UNCLASSIFIED")
 
-#Define missing value strings
-missing <- c("#############","NULL")
-
-#XLSX import function
-import <- function(dataFile) {
-    read_xlsx(dataFile,col_names=meta$Names,col_types=meta$Type,skip=1,
-        na=missing)    
+pre2015Import <- function(dataFile) {
+    raw <- read_xlsx(dataFile,col_names=TRUE,guess_max=10000,na=pre2015Missing)
+    names(raw) <- str_replace_all(toupper(names(raw)),fixed(" "),"_")
+    raw <- mutate_all(raw,c("toupper"))
+    return(raw)
 }
 
-#Import files into list
-PERMALLlist <- vector("list",length(dataFiles))
-for (i in seq_along(dataFiles)) {
-    PERMALLlist[[i]] <- import(dataFiles[[i]])
+PERM2008 <- pre2015Import(pre2015DataFiles[1])
+PERM2009 <- pre2015Import(pre2015DataFiles[2])
+PERM2010 <- pre2015Import(pre2015DataFiles[3])
+PERM2011 <- pre2015Import(pre2015DataFiles[4])
+PERM2012 <- pre2015Import(pre2015DataFiles[5])
+PERM2013 <- pre2015Import(pre2015DataFiles[6])
+PERM2014 <- pre2015Import(pre2015DataFiles[7])
+
+pre2015 <- rbind.fill(PERM2008,PERM2009,PERM2010,PERM2011,PERM2012,PERM2013,
+    PERM2014)
+
+post2015Meta <- read_xlsx("PermDisclosureData/meta.xlsx",
+    col_names=c("Names","Type"),col_types="text",skip=1)
+
+post2015DataFiles <- dir("PermDisclosureData",pattern="[2][0][1][5-8].xlsx"
+    ,full.names=TRUE)
+
+post2015Missing <- c("#############","NULL")
+
+post2015Import <- function(dataFile) {
+    raw <- read_xlsx(dataFile,col_names=post2015Meta$Names,col_types=post2015Meta$Type
+        ,skip=1,na=post2015Missing)    
+    names(raw) <- toupper(names(raw))
+    raw <- mutate_all(raw,c("toupper"))
 }
 
-#Collapse list to tibble
-PERMALL <- bind_rows(PERMALLlist)
+post2015List <- vector("list",length(post2015DataFiles))
+for (i in seq_along(post2015DataFiles)) {
+    post2015List[[i]] <- post2015Import(post2015DataFiles[[i]])
+}
 
-#Convert text columns to factors (other types preserved)
-PERMALLDF <- as.data.frame(unclass(PERMALL),stringsAsFactors = TRUE)
+post2015 <- bind_rows(post2015List)
 
-#Count number of levels
+PERMALL <- rbind.fill(pre2015,post2015)
+
+PERMALLtrans1 <- PERMALL %$%
+    mutate(.,EmployerState = mapvalues(EMPLOYER_STATE,toupper(state.name),
+        state.abb)) %$%
+    mutate(.,JobState = mapvalues(JOB_INFO_WORK_STATE,toupper(state.name),
+        state.abb)) %$%
+    mutate(.,SOCMajor = str_sub(PW_SOC_CODE,1,2))
+
+PERMALLDF <- as.data.frame(unclass(PERMALLtrans1),stringsAsFactors = TRUE)
+
 numLevels <- lapply(PERMALLDF, function(x) nlevels(x))
+pctMissing <- lapply(PERMALLDF, function(x) sum(is.na(x))/nrow(PERMALLDF))
 
-PERMALL %>%
-    select(which(numLevels < 2000)) %>%
+PERMALLtrans1 <- PERMALLDF %>%
+    select(which(pctMissing < 0.5 & numLevels < 300)) %>%
+    filter(CASE_STATUS %in% c("CERTIFIED","CERTIFIED-EXPIRED","DENIED")) %$%
+    mutate(.,Denied = revalue(CASE_STATUS, c("CERTIFIED"=0,
+        "CERTIFIED-EXPIRED"=0,"DENIED"=1)))
     
-    #Select columns with less then 50% missing
-    select(which(colMeans(is.na(.)) < 0.5)) %>%
-    
-    #Filter out withdrawn observations
-    filter(CASE_STATUS %in% c("Certified","Certified-Expired","Denied")) %$%
-    
-    #Revalue "Certified-Expired" and add as new variable
-    mutate(.,status = revalue(CASE_STATUS,
-        c("Certified-Expired" = "Certified"))) %$%
-    
-    #EXtract SOC major group and add as new variable
-    mutate(.,SOCMajorGroup = str_sub(PW_SOC_CODE,1,2))
-
-    #Test comment
     
     
     
